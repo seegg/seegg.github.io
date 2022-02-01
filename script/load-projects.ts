6e062cf5b49fc0de784ceb5f9cc7c00a979058b3
yuan tan <dyxtan@gmail.com>
2022-02-02 01:45:09 +1300

    fix repo link in loadproject

--- script/load-projects.ts
+++ script/load-projects.ts
@@ -1,10 +1,59 @@
 import { getProjects } from "./data";
+import { Project } from "./types";
+
+const contentWrapper = document.getElementById('content');
 
 export const loadProjects = () => {
-  console.log(getProjects());
+  if (contentWrapper) {
+    console.log('loading projects');
+    getProjects().forEach(project => {
+      const view = createProjectComponent(project);
+      contentWrapper.appendChild(view);
+    })
+  }
 }
 
 
-const createProjectComponent = (): HTMLElement => {
+const createProjectComponent = (project: Project): HTMLElement => {
+  //outer article 
+  const projectContainer = document.createElement('article');
+  projectContainer.classList.add('project-container');
 
+  //link to repo with github logo
+  const repoLink = document.createElement('nav');
+  repoLink.classList.add('github-link');
+
+  const repoAnchor = document.createElement('a');
+  const gitHLogo = new Image();
+  gitHLogo.src = "http://localhost:8000/images/GitHub-Mark-Light-32px.png"
+  gitHLogo.classList.add('github-logo');
+  repoAnchor.appendChild(gitHLogo);
+  repoLink.appendChild(repoAnchor);
+  projectContainer.appendChild(repoLink);
+
+  //container for the project details
+  const secondArticle = document.createElement('article');
+  secondArticle.classList.add('project');
+
+  // image for the project
+  const imgWrapper = document.createElement('div');
+  const projectImg = new Image();
+  projectImg.src = project.img;
+  projectImg.classList.add('project-img');
+  imgWrapper.appendChild(projectImg);
+  secondArticle.appendChild(imgWrapper);
+
+  //inner article for descriptions
+  const innerArticle = document.createElement('article');
+  innerArticle.classList.add('project-description');
+  innerArticle.innerHTML = `<h4 class="project-title">${project.name}</h4><p>${project.description}</p>`
+  secondArticle.appendChild(innerArticle);
+
+  //if the project has a site 
+  if (project.url) {
+    secondArticle.innerHTML = `<a href="${project.url}">${secondArticle.innerHTML}</a>`
+  }
+  projectContainer.appendChild(secondArticle);
+
+  return projectContainer;
 }
--- script/types.ts
+++ script/types.ts
@@ -2,5 +2,6 @@ export interface Project {
   name: string,
   url: string | null,
   repo: string | null,
-  img: string | null,
+  img: string,
+  description: string
 }
