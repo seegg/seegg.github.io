b188e9fea3c36b82ad3e2ba62868835a9bbddebb
yuan tan <dyxtan@gmail.com>
2022-03-16 19:57:08 +1300

    make scrolling smooth when closing deck

--- script/collapseDeckOnScroll.ts
+++ script/collapseDeckOnScroll.ts
@@ -3,6 +3,7 @@ const projectContainer = document.getElementById('projects');
 const navBar = document.getElementById('tab-nav-bar');
 const navBarFiller = document.getElementById('nav-filler');
 const fixedNavBar = 'tab-nav-fixed';
+const closeCard = 'close-deck-height';
 
 /**
  * 
@@ -10,16 +11,41 @@ const fixedNavBar = 'tab-nav-fixed';
  */
 export const collapseDeckOnScroll = (maxWidth: number) => {
   const projectCards = Array.from(document.getElementsByClassName('project-card')) as HTMLElement[];
+  let currentIndex = 0;
+  let prevScrollY = window.scrollY;
 
+  let prevTime = 0;
   document.addEventListener('scroll', () => {
 
-    if (contentContainer.getBoundingClientRect().top <= 0) {
-      toggleNavBarFixedPosition('fixed')
-      console.log(navBar);
+    const top = contentContainer.getBoundingClientRect().top;
+    const scrollYDiff = window.scrollY - prevScrollY;
+    console.log('scrolling');
+    if (top <= 0) {
+      toggleNavBarFixedPosition('fixed');
+      const currentTime = new Date().getTime();
+      if (currentTime - prevTime >= 500) {
+        if (scrollYDiff > 0) {
+          console.log('triggered');
+          prevTime = currentTime;
+          projectCards[currentIndex].classList.add(closeCard);
+          currentIndex++;
+        } else {
+          projectCards[currentIndex].classList.remove(closeCard);
+          currentIndex--;
+        }
+        window.scrollTo({
+          top: 220,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
 
     } else {
       toggleNavBarFixedPosition('not-fixed');
     }
+
+    prevScrollY = window.scrollY;
+
   })
 
 }
